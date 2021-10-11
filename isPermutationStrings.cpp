ecc32018a466dfeaf5c14149e8a0c8dce661104c
Chen Lu <59344011+ChenLu1211@users.noreply.github.com>
2021-10-11 12:50:27 +0300

    Create isPermutationStrings.cpp

---
+++ isPermutationStrings.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+Given two strings, write a method to decide if one is a permutation of the
+other.
+with space- hash \ bucket
+without space- mergesort \ bubble
+I will use hash -> multi-set.
+*/
+
+bool isPermutation(string first, string second);
+
+int main()
+{
+	string first, second;
+	cin >> first >> second;
+	if (isPermutation(first, second)) {
+		cout<< "True";
+	}
+	else {
+		cout<<"False";
+	}
+	
+	return 0;
+}
+
+bool isPermutation(string first, string second)
+{
+	if (first.length() != second.length()) {
+		return false;
+	}
+	int len = first.length();
+
+	multiset<char> myStr;
+	for (int i = 0; i < len; i++)
+	{
+		myStr.insert(first[i]);
+	}
+
+	for (int j = 0; j < len; j++)
+	{
+		if (myStr.count(second[j]))
+		{
+			myStr.erase(myStr.find(second[j]));
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+}
